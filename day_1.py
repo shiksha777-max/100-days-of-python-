f5157aa59526e7927ba06b0fb382e3120e4f680d
shiksha777-max <shikshanw@gmail.com>
2026-06-26 17:17:10 +0545

    Add files via upload

---
+++ day_1.py
@@ -0,0 +1,4 @@
+print("Hello, " + input("What is your name? ") + "!") # type: ignore
+city=input("What city do you live in? ")
+pet=input("What is the name of your pet? ")
+print("Your band name could be " + city + " " + pet)
